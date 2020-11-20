821d7d20415cb40c47e5d1023cca828e81bbc49c
oakideas <5075187+oakideas@users.noreply.github.com>
2020-11-20 19:09:03 -0300

    initial commit

---
+++ src/JsonFileTarget.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace fabiooak\yii2\log;
+
+use yii\log\FileTarget;
+use yii\log\Logger;
+use yii\helpers\Json;
+
+/**
+ * JsonFileTarget extends FileTarget changing the output to JSON
+ *
+ * @author Fabio Oak <fabio@oakideas.com.br>
+ */
+class JsonFileTarget extends FileTarget
+{
+    /**
+     * Formats a log message for display as a JSON string.
+     * @param array $message the log message to be formatted.
+     * @return string the formatted message
+     */
+    public function formatMessage($message): string {
+
+        list($text, $level, $category, $timestamp) = $message;
+        $level = Logger::getLevelName($level);
+
+        if ($text instanceof \Throwable || $text instanceof \Exception) {
+            $text = (string) $text;
+        }
+
+        $traces = [];
+        if (isset($message[4])) {
+            foreach ($message[4] as $trace) {
+                $traces[] = "in {$trace['file']}:{$trace['line']}";
+            }
+        }
+
+        return Json::encode(['timestamp' => $timestamp, 'datetime' => date('Y-m-d\TH:i:s\Z', intval($timestamp)), 'text' => $text, 'level' => $level, 'category' => $category, 'traces' => $traces]);
+    }
+}
